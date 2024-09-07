a2a6b5893e51f8e3640adecb7b9c9c3ed7f5355d
rburgos <rachel@pixelstreetmedia.com>
2024-09-07 15:19:59 -0500

    initial push

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', //If a category is deleted, delete associated products
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+  
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+    foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
@@ -18,3 +33,4 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
