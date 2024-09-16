1e76ea2a7cc1d2304ef7dd19f09fae6d706960f9
rburgos <rachel@pixelstreetmedia.com>
2024-09-16 17:07:11 -0500

    tag-routes added and other modifications.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# E-commerce Back End Starter Code
+# E-commerce Back End Starter Code# Object-Relational Mapping (ORM) Challenge: E-commerce Back End
+
+## Description
+
+- This Challenge is to build the back end for an e-commerce site. You’ll take a working Express.js API and configure it to use Sequelize to interact with a PostgreSQL database.
+
+## Tasks Completed
+
+-- Added credentials, my database name, PostgreSQL username, and PostgreSQL password to .env file
+-- Created schema, models and seed commands
+-- Developed API GET routes in Insomnia Core for categories, products, or tags
+-- Developemed API POST, PUT, and DELETE routes and tested in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+
+## Installation
+
+- clone the repo
+
+- install npm using `npm i` in terminal
+
+- seed the database with 'npm run seed' in terminal
+
+- run application using `npm start'
+
+## Usage
+
+![NOTE TAKER APPLICATION screenshot](public/assets/images/screenshot_1.png)
+![NOTE TAKER APPLICATION screenshot](public/assets/images/screenshot_2.png)
+
+## Credits
+
+\*\* This project was accomplished with the help provided by the instructors and TAs of the Rice University Coding Bootcamp, including Instructor Darian Mendez, Mateo Wallace, Mark Alfano, Gerard Mennella
+
+Starter code was used to begin this project, which can be found at:
+
+-- E-commerce Back End Starter Code <https://github.com/coding-boot-camp/bookish-sniffle>
+
+References:
+<>
+
+## License
+
+- LicenseDistributed under the MIT License. See LICENSE.txt for more information.
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,93 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// GET all tags
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'], // select tag ID and tag name
+    include: [
+      {
+        model: Product, // include associated products
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'], // select product fields
+      },
+    ],
+  })
+    .then((data) => {
+      res.json(data); // respond with all found tags and their products
+    })
+    .catch((err) => {
+      res.status(400).json(err); // handle errors and send 400 status code
+    });
 });
 
+// GET a single tag by id
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id, // get tag id from URL parameters
+    },
+    attributes: ['id', 'tag_name'], // select tag ID and tag name
+    include: [
+      {
+        model: Product, // include associated products
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'], // select product fields
+      },
+    ],
+  })
+    .then((data) => {
+      res.json(data); // respond with the tag and its associated products
+    })
+    .catch((err) => {
+      res.status(400).json(err); // handle errors and send 400 status code
+    });
 });
 
+// POST a new tag
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({ tag_name: req.body.tag_name }) // create a new tag with the name provided in the request body
+    .then((data) => {
+      res.json(data); // respond with the created tag
+    })
+    .catch((err) => {
+      res.status(400).json(err); // handle errors and send 400 status code
+    });
 });
 
+// PUT (update) a tag's name by id
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id, // get the tag id from URL parameters
+    },
+  })
+    .then((data) => {
+      res.json(data); // respond with the updated tag
+    })
+    .catch((err) => {
+      res.status(400).json(err); // handle errors and send 400 status code
+    });
 });
 
+// DELETE a tag by id
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete one tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id, // get the tag id from URL parameters
+    },
+  })
+    .then((data) => {
+      res.json(data); // respond with the result of the deletion
+    })
+    .catch((err) => {
+      res.status(400).json(err); // handle errors and send 400 status code
+    });
 });
 
 module.exports = router;
